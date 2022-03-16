7b7434d06fe34ce4292f5782b8dff99e83c7a8f7
AlexPron <Fotolinza@gmail.com>
2022-03-16 17:18:41 +0300

    lesson19_1.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
+        <BrowserRouter>  //должен обромлять  Route
         <div className="app-wrapper">
             <Header type1={"Это заголовок"}/>
-            <Navbar type2={"Это меню"}/>
-            <Profile type3={"Это сам сайт"}/>
-
-
-
+            <Navbar textNavbar={"Menu"}/>
+            <div className="app-wrapper-content">
+                <Route path='/Dialogs' component={Dialogs} />
+                <Route path='/profile' component={Profile}/>
+            </div>
         </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,7 @@
 
 import React from 'react';
 import s from "./Navbar.module.css";
-// let s = {
-//     'nav': 'Navbar_nav__00e8L',
-//     'item': 'Navbar_item__SgK1I'
-//      'active': 'Bgedgl_active'
-// }
+
 console.log(s);
 let c1 = "item";
 let c2 = "active";
@@ -14,20 +10,20 @@ let classes = c1 + " " + c2;
 let classesNew = `${s.item} ${s.active}`;
 
 type NavbarPropsType = {
-    type2: string;
+    textNavbar: string;
 }
 const Navbar = (props: NavbarPropsType) => {
-
+debugger
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                {props.type2}
+                {props.textNavbar}
             </div>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <a href="/modules">Messages</a>
             </div>
             <div className={s.item}>
                 <a>News</a>
