d84e3d6b72f6a9301e7755cae644db5fb50f81b3
AlexPron <Fotolinza@gmail.com>
2022-03-16 23:28:12 +0300

    lesson21-1

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-           Text of the dialogs
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className="item">
+                    Dimych
+                </div>
+                <div className="item">
+                    Andrey
+                </div>
+                <div className="item">
+                    Sveta
+                </div>
+                <div className="item">
+                    Sasha
+                </div>
+                <div className="item">
+                    Viktor
+                </div>
+                <div className="item">
+                    Valera
+                </div>
+            </div>
+        </div>
+        <div className="messages">
         </div>
     );
 };
