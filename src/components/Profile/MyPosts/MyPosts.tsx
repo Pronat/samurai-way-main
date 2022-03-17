1d27ce85dd0be492cf57adaf7336042727db7120
AlexPron <Fotolinza@gmail.com>
2022-03-17 22:37:01 +0300

    lesson22-5

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,24 +3,30 @@ import style from './MyPosts.module.css';
 import Post from "./Posts/Post";
 
 
-
 const MyPosts = () => {
     debugger
     return (
-        <div>
-            <textarea></textarea>
-            <button>Add post</button>
+        <div className={style.postsBlock}>
+            <h3>My Posts</h3>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
+            </div>
             <div className={style.item}>
             </div>
-                <div className={style.item}>
-                    <Post message={"It's my first post"} likesCount={4} />
-                    <Post message={"Hello, how are you"} likesCount={10}/>
-                    <Post message={"What happening"} likesCount={2}/>
-                    <Post message={"What would we do?"} likesCount={18}/>
-                </div>
+            <div className={style.item}>
+                <Post message={"It's my first post"} likesCount={4}/>
+                <Post message={"Hello, how are you"} likesCount={10}/>
+                <Post message={"What happening"} likesCount={2}/>
+                <Post message={"What would we do?"} likesCount={18}/>
+            </div>
         </div>
-                )
-                }
+    )
+}
 
 
-                export default MyPosts;
+export default MyPosts;
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Profile.module.css';
+import s from './ProfileInfo.module.css';
 
 
 const ProfileInfo = () => {
@@ -11,7 +11,7 @@ const ProfileInfo = () => {
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                     alt={"no Photo"}/>
             </div>
-            <div>
+            <div className={s.descriptionBlock}>
                 ava + description
             </div>
         </div>
