5d64f2663854727f69a658428a5bd25d444031f0
AlexPron <Fotolinza@gmail.com>
2022-03-26 23:20:33 +0300

    lesson24-1

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -28,12 +28,28 @@ type DialogsPropsType = {
 }
 
 export const Dialogs = (props: DialogsPropsType) => {
+    let dialogsData = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Andrey"},
+        {id: 3, name: "Sveta"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Viktor"},
+        {id: 6, name: "Valera"},
+    ]
+
+    let messagesData = [
+        {id: 1, message: "Hi"}
+        {id: 1, message: "How is your it-kamasutra?"}
+        {id: 1, message: "Yo"}
+        {id: 1, message: "How are you"}
+        {id: 1, message: "Call me"}
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
 
-                <DailogItem name="Dimych" id={1}/>
-                <DailogItem name="Andrey" id={2}/>
+                <DailogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DailogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
                 <DailogItem name="Sveta" id={3}/>
                 <DailogItem name="Sasha" id={4}/>
                 <DailogItem name="Viktor" id={5}/>
@@ -42,8 +58,8 @@ export const Dialogs = (props: DialogsPropsType) => {
             </div>
 
             <div className={s.messages}>
-                <Message message="Hi" />
-                <Message message="How is your it-kamasutra?" />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
                 <Message message="Yo" />
                 <Message message="How are you" />
                 <Message message="Call me" />
