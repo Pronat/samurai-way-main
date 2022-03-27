b656381c76463a3232a83bea731212d56efb753b
AlexPron <Fotolinza@gmail.com>
2022-03-27 16:13:59 +0300

    lesson26-1

---
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -0,0 +1,15 @@
+import s from "../Dialogs.module.css";
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+type DialogItemPropsType = {
+    name: string;
+    id: number;
+}
+export const DialogItem = (props: DialogItemPropsType) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,12 @@
+import s from "../Dialogs.module.css";
+import React from "react";
+
+type MessagePropsType = {
+    message: string
+    id: number
+}
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
