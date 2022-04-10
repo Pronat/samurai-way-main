d1b50d698e29f4a2805d5d111c1abb1a7410d63e
AlexPron <Fotolinza@gmail.com>
2022-04-10 21:18:56 +0300

    lesson30-1

--- src/redax/state.tsx
+++ src/redax/state.tsx
@@ -1,31 +1,5 @@
 import React from 'react';
 
-// let posts: Array<PostsType> = [
-//     {id:1, message:"It's my first post", likesCount:4},
-//     {id:2, message:"Hello, how are you", likesCount:10},
-//     {id:3, message:"What happening", likesCount:2},
-//     {id:4, message:"What would we do?", likesCount:18},
-//     {id:5, message:"Here is my moto!", likesCount:18},
-// ]
-//
-// let dialogs:Array<DialogsType> = [
-//     {id: 1, name: "Dimych"},
-//     {id: 2, name: "Andrey"},
-//     {id: 3, name: "Sveta"},
-//     {id: 4, name: "Sasha"},
-//     {id: 5, name: "Viktor"},
-//     {id: 6, name: "Valera"},
-// ]
-//
-// let messages:Array<MessagesType> = [
-//     {id: 1, message: "Hi"},
-//     {id: 1, message: "How is your it-kamasutra?"},
-//     {id: 1, message: "Yo"},
-//     {id: 1, message: "How are you"},
-//     {id: 1, message: "Call me"},
-// ]
-
-
 export type PostsType = {
     id: number
     message: string
@@ -46,6 +20,7 @@ export type MessagesPageType = {
     messages: Array<MessagesType>
     dialogs: Array<DialogsType>
 }
+// type sidebar={}
 export type RootStateType = {
     profilePage:ProfilePageType
     messagesPage:MessagesPageType
@@ -77,5 +52,6 @@ export let state: RootStateType = {
             {id: 5, name: "Viktor"},
             {id: 6, name: "Valera"},
         ],
+        // sidebar: {}
     }
 }
