8a39615b7303f122199bc0dd02e11089e19db118
AlexPron <Fotolinza@gmail.com>
2022-04-12 22:39:48 +0300

    lesson33-1

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import {addPost, state} from "./redax/state";
 
-
-
+let rerenderEntireTree = () => {
 ReactDOM.render(
     <App state={state} addPost={addPost}/>,
   document.getElementById('root')
-
 );
+}
