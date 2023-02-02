c89064b404ee74fea5af684606bdf2a6a2f17ca0
AlexPron <Fotolinza@gmail.com>
2023-02-02 20:11:38 +0300

    less7-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <ol>
+         <li>React</li>
+         <li>HTML</li>
+         <li>CSS</li>
+         <li>Redux</li>
+     </ol>
     </div>
   );
 }
