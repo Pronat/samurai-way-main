1b89cfb5c0b58fb87175fe1c537d7d80aca365ed
AlexPron <Fotolinza@gmail.com>
2023-02-03 19:27:53 +0300

    less7-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,33 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-     <ol>
-         <li>React</li>
-         <li>HTML</li>
-         <li>CSS</li>
-         <li>Redux</li>
-     </ol>
+        <Technologies />
     </div>
   );
+}
+
+const Technologies = () => {
+
+    return (
+        <div>
+            <ol>
+                <li>React</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Redux</li>
+            </ol>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href={'#'}>Home</a>
+            <a href={'#'}>Need Feed</a>
+            <a href={'#'}>Messages</a>
+        </div>
+    )
 }
 
 export default App;
