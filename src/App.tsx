b66c39c637c1eeece7a28ab94c7430431a366239
AlexPron <Fotolinza@gmail.com>
2023-02-04 09:01:38 +0300

    less11-2

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import './App.css';
 
 
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                    <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgqQe8JhxoXjy2_lve779pbB0AlaR-rm4bHA&usqp=CAU'} />
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Contacts</div>
+            </nav>
+            <div>
+                Main Content
+            </div>
+        </div>
+    );
 }
 
 export default App;
