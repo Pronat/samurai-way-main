8d48dcf375b93588d7a79d5c231cdd051b776909
AlexPron <Fotolinza@gmail.com>
2023-02-04 21:33:15 +0300

    less15-5

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,20 +4,21 @@ import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     return (
-        <div>
-            <div className={s.posts}>
-                <h2>My Posts</h2>
+        <div className={s.posts}>
+            <h2>My Posts</h2>
+            <div>
+                <textarea/>
                 <div>
-                    <textarea />
-                    <div>
-                        <button>Create post</button>
-                    </div>
+                    <button>Create post</button>
                 </div>
-                <img className={s.avatarImg} src= 'https://www.blast.hk/attachments/64805/' />
-                <Post title={'Post number 1'}/>
-                <Post title={'Post number 2'}/>
-                <Post title={'Post number 3'}/>
             </div>
+            <div className={s.item}>
+                <img src={' https://www.blast.hk/attachments/64805/'} />
+            </div>
+
+            <Post title={'Post number 1'}/>
+            <Post title={'Post number 2'}/>
+            <Post title={'Post number 3'}/>
         </div>
     )
 }
