04f42011ca0ffef92de854dc23bb5fb25b12aabd
AlexPron <Fotolinza@gmail.com>
2023-02-04 20:37:27 +0300

    less15-3

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -8,8 +8,12 @@ export const MyPosts = () => {
             <div className="posts">
                 My Posts
                 <div>
-                    New post
+                    <textarea />
+                    <div>
+                        <button>Create post</button>
+                    </div>
                 </div>
+                <img className={s.avatarImg} src= 'https://www.blast.hk/attachments/64805/' />
                 <Post title={'Post number 1'}/>
                 <Post title={'Post number 2'}/>
                 <Post title={'Post number 3'}/>
