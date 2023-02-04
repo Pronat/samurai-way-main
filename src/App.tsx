877e82e66edb4b3e0e5d2c56a73ddcaeb26ebe7e
AlexPron <Fotolinza@gmail.com>
2023-02-04 10:10:54 +0300

    less11-7

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,21 @@ const App = () => {
                 </div>
             </nav>
             <div className="content">
-                <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" />
+                <div>
+                    <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" />
+                </div>
+                <div>
+                     <img style={{width: '130px'}} src="https://www.photoshopbuzz.com/wp-content/uploads/change-color-part-of-image-psd4.jpg" />
+                </div>
+                <div>
+                    My Posts
+                    <div>
+                        New post
+                    </div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                </div>
             </div>
         </div>
     );
