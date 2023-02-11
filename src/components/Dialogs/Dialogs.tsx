844451dc58a3053c441d596ac7ee5c41574b23c6
AlexPron <Fotolinza@gmail.com>
2023-02-11 13:54:27 +0300

    less21-1

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -8,13 +8,13 @@ const Dialogs = (props: DialogsType) => {
     return (
         <div className={s.dialogsPage}>
             <div className={s.names}>
-                <div className={s.name}>Alex</div>
-                <div className={s.name}>Svetlana</div>
-                <div className={s.name}>Burton</div>
-                <div className={s.name}>Olaf</div>
-                <div className={s.name}>Olga</div>
-                <div className={s.name}>Brendon</div>
-                <div className={s.name}>Nikita</div>
+                <div className={`${s.name} ${s.active}`}>Alex</div>
+                <div className={`${s.name} ${s.active}`}>Svetlana</div>
+                <div className={`${s.name} ${s.active}`}>Burton</div>
+                <div className={`${s.name} ${s.active}`}>Olaf</div>
+                <div className={`${s.name} ${s.active}`}>Olga</div>
+                <div className={`${s.name} ${s.active}`}>Brendon</div>
+                <div className={`${s.name} ${s.active}`}>Nikita</div>
             </div>
 
             <div className={s.messages}>
