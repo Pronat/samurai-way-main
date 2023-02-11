f44686256ac23d75dd31cc46580a6c2e4bc35b0d
AlexPron <Fotolinza@gmail.com>
2023-02-11 11:17:25 +0300

    less19-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route, Switch} from "react-router-dom";
 
 
 export const App = () => {
@@ -12,7 +13,11 @@ export const App = () => {
             <Header/>
             <NavBar/>
             <div className={s.AppWrapperContent}>
-                {/*<Profile />*/}
+                <Switch>
+                    <Route path={'/profile'} element={<Profile/> } />
+                    <Route path={'/message'} element={<Dialogs/> } />
+                </Switch>
+                <Profile />
                 <Dialogs/>
             </div>
         </div>
