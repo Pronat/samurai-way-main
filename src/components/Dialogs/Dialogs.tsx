138827910a4beef052ad76dcf37d2bae88f37538
AlexPron <Fotolinza@gmail.com>
2023-02-11 13:45:50 +0300

    less19-9

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,8 +6,25 @@ type DialogsType = {
 }
 const Dialogs = (props: DialogsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogsPage}>
+            <div className={s.names}>
+                <div className={s.name}>Alex</div>
+                <div className={s.name}>Svetlana</div>
+                <div className={s.name}>Burton</div>
+                <div className={s.name}>Olaf</div>
+                <div className={s.name}>Olga</div>
+                <div className={s.name}>Brendon</div>
+                <div className={s.name}>Nikita</div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hello my friend</div>
+                <div className={s.message}>What are you doing?</div>
+                <div className={s.message}>What time is it?</div>
+                <div className={s.message}>Do you have any problems?</div>
+                <div className={s.message}>What did you say</div>
+            </div>?
+
         </div>
     );
 };
