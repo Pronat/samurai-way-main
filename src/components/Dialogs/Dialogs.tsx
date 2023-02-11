249f09b08958a911e83f1499391afda5f4b79b2e
AlexPron <Fotolinza@gmail.com>
2023-02-11 15:42:37 +0300

    less23-3

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-type DialogsType = {
-}
+type DialogsType = {}
 type DialogType = {
     name: string
     id: number
@@ -13,36 +12,22 @@ export const Dialog = (props: DialogType) => {
         <div className={`${s.name} ${s.active}`}>
             <NavLink to={`/message/${props.id}`}>{props.name}</NavLink>
         </div>
-        )
+    )
 }
 const Dialogs = (props: DialogsType) => {
     return (
         <div className={s.dialogsPage}>
-            <Dialog name={'Alex'} id={1} />
 
-            {/*<div className={s.names}>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/1'}>Alex</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/2'}>Svetlana</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/3'}>Burton</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/4'}>Olaf</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/5'}>Olga</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/6'}>Brendon</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={`${s.name} ${s.active}`}>*/}
-            {/*        <NavLink to={'/message/7'}>Nikita</NavLink>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+
+            <div className={s.names}>
+                <Dialog name={'Alex'} id={1}/>
+                <Dialog name={'Svetlana'} id={2}/>
+                <Dialog name={'Burton'} id={3}/>
+                <Dialog name={'Olaf'} id={4}/>
+                <Dialog name={'Olga'} id={5}/>
+                <Dialog name={'Nikita'} id={6}/>
+                <Dialog name={'Brendon'} id={7}/>
+            </div>
 
             <div className={s.messages}>
                 <div className={s.message}>Hello my friend</div>
