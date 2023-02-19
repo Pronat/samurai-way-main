146c056fe5cd37fe526a61e99b3b096ef0bc52ec
AlexPron <Fotolinza@gmail.com>
2023-02-19 21:10:26 +0300

    less25-4

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,21 +9,19 @@ export const MyPosts = () => {
         {id: 2, message: 'Where are you?', likeCount: 56},
         {id: 3, message: 'I need new phone', likeCount: 2},
         {id: 4, message: 'What did you say?', likeCount: 99},
-
     ]
+    let postsElements =  PostsData.map((el)=> {
+        return (
+            <Post message={el.message} likeCount={el.likeCount}/>
+        )
+    } )
+
     return (
         <div className={s.posts}>
 
             <ProfileInfo />
-            {/*<Post message={'Hello, it is my first post'} likeCount={23}/>*/}
-            {/*<Post message={'Where are you?'} likeCount={4}/>*/}
-            {/*<Post message={'I need a new phone'} likeCount={12}/>*/}
             {
-                PostsData.map((el)=> {
-                    return (
-                        <Post message={el.message} likeCount={el.likeCount}/>
-                    )
-                } )
+                postsElements
             }
 
         </div>
