9b5513c8245a3eb6094f7fde106b7c0db035d669
AlexPron <Fotolinza@gmail.com>
2023-03-06 16:45:08 +0300

    less35-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
 import './index.css';
-
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {addMessage, addPost, StatePropsType, updateNewMessageText, updateNewPostText} from "./redux/state";
 import {state} from "./redux/state";
-import {rerenderEntireTree} from "./render";
 
+
+export const rerenderEntireTree = (state: StatePropsType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App
+                state={state}
+                addPost={addPost}
+                updateNewPostText={updateNewPostText}
+                addMessage={addMessage}
+                updateNewMessageText={updateNewMessageText}
+            />
+        </BrowserRouter>
+        ,
+        document.getElementById('root')
+    );
+}
 
 
 
--- src/render.tsx
+++ src/render.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {addMessage, addPost, StatePropsType, updateNewMessageText, updateNewPostText} from "./redux/state";
-
-
-export const rerenderEntireTree = (state: StatePropsType) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App
-                state={state}
-                addPost={addPost}
-                updateNewPostText={updateNewPostText}
-                addMessage={addMessage}
-                updateNewMessageText={updateNewMessageText}
-            />
-        </BrowserRouter>
-        ,
-        document.getElementById('root')
-    );
-}
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import {BrowserRouter} from "react-router-dom";
+// import {addMessage, addPost, StatePropsType, updateNewMessageText, updateNewPostText} from "./redux/state";
+//
+//
+// export const rerenderEntireTree = (state: StatePropsType) => {
+//     ReactDOM.render(
+//         <BrowserRouter>
+//             <App
+//                 state={state}
+//                 addPost={addPost}
+//                 updateNewPostText={updateNewPostText}
+//                 addMessage={addMessage}
+//                 updateNewMessageText={updateNewMessageText}
+//             />
+//         </BrowserRouter>
+//         ,
+//         document.getElementById('root')
+//     );
+// }
