35f5ea83ad2d4339d631a74d5336b4ef3bf61bce
AlexPron <Fotolinza@gmail.com>
2023-03-06 19:29:38 +0300

    less36-2

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -28,77 +28,139 @@ export type SiteBarType = {
     id: number
     name: string
 }
-
-export const store = {
-
-}
-
-
-
 export type StatePropsType = {
     postsPage: PostsPageType
     profilePage: ProfilePageType
     siteBar: Array<SiteBarType>
 }
-export let state = <StatePropsType>{
-    profilePage: <ProfilePageType>{
-        DialogsData: <Array<DialogsDataType>>[
-            {id: 1, name: 'Alex'},
-            {id: 2, name: 'Svetlana'},
-            {id: 3, name: 'Burton'},
-            {id: 4, name: 'Olaf'},
-            {id: 5, name: 'Olga'},
-            {id: 6, name: 'Nikita'},
-            {id: 7, name: 'Brendon'},
-        ],
-        messages: <Array<MessagesType>>[
-            {id: 1, message: 'Hello my friend'},
-            {id: 2, message: 'What are you doing?'},
-            {id: 3, message: 'What time is it?'},
-            {id: 4, message: 'Do you have any problems?'},
-            {id: 5, message: 'What did you say?'},
-        ],
-        newMessageText: 'Enter your message'
+
+export const store = {
+    state: <StatePropsType>{
+        profilePage: <ProfilePageType>{
+            DialogsData: <Array<DialogsDataType>>[
+                {id: 1, name: 'Alex'},
+                {id: 2, name: 'Svetlana'},
+                {id: 3, name: 'Burton'},
+                {id: 4, name: 'Olaf'},
+                {id: 5, name: 'Olga'},
+                {id: 6, name: 'Nikita'},
+                {id: 7, name: 'Brendon'},
+            ],
+            messages: <Array<MessagesType>>[
+                {id: 1, message: 'Hello my friend'},
+                {id: 2, message: 'What are you doing?'},
+                {id: 3, message: 'What time is it?'},
+                {id: 4, message: 'Do you have any problems?'},
+                {id: 5, message: 'What did you say?'},
+            ],
+            newMessageText: 'Enter your message'
+        },
+        postsPage: <PostsPageType>{
+            PostsData: <Array<PostsDataType>>[
+                {id: 1, message: 'Hello, it is my first post', likeCount: 23},
+                {id: 2, message: 'Where are you?', likeCount: 56},
+                {id: 3, message: 'I need new phone', likeCount: 2},
+                {id: 4, message: 'What did you say?', likeCount: 99},
+            ],
+            newPostText: 'Enter your post'
+        },
+        siteBar: <Array<SiteBarType>>[
+            {id: 1, name: 'Tom'},
+            {id: 2, name: 'Clark'},
+            {id: 3, name: 'Kent'},
+        ]
     },
-    postsPage: <PostsPageType>{
-        PostsData: <Array<PostsDataType>>[
-            {id: 1, message: 'Hello, it is my first post', likeCount: 23},
-            {id: 2, message: 'Where are you?', likeCount: 56},
-            {id: 3, message: 'I need new phone', likeCount: 2},
-            {id: 4, message: 'What did you say?', likeCount: 99},
-        ],
-        newPostText: 'Enter your post'
+
+     addPost() {
+        const newPost =  {id:  state.postsPage.PostsData.length+1, message: state.postsPage.newPostText, likeCount: 0}
+        state.postsPage.PostsData.push(newPost)
+        // updateNewPostText('')
+        state.postsPage.newPostText = ''
+        rerenderEntireTree(state)
     },
-    siteBar: <Array<SiteBarType>>[
-        {id: 1, name: 'Tom'},
-        {id: 2, name: 'Clark'},
-        {id: 3, name: 'Kent'},
-    ]
+     updateNewPostText(newText: string) {
+        state.postsPage.newPostText = newText
+        rerenderEntireTree(state)
+    },
+
+     addMessage() {
+        const newMessage = {id: state.profilePage.messages.length+1, message: state.profilePage.newMessageText}
+        state.profilePage.messages.push(newMessage)
+        state.profilePage.newMessageText = ''
+        rerenderEntireTree(state)
+    },
+     updateNewMessageText(newMessage: string) {
+        state.profilePage.newMessageText = newMessage
+        rerenderEntireTree(state)
+    },
+
+    subscribe(observer: (state: StatePropsType) => void) {
+        rerenderEntireTree = observer
+    }
 }
 
-export const addPost = () => {
-    const newPost =  {id:  state.postsPage.PostsData.length+1, message: state.postsPage.newPostText, likeCount: 0}
-    state.postsPage.PostsData.push(newPost)
-    // updateNewPostText('')
-    state.postsPage.newPostText = ''
-    rerenderEntireTree(state)
-}
-export const updateNewPostText = (newText: string) => {
-    state.postsPage.newPostText = newText
-    rerenderEntireTree(state)
-}
 
-export const addMessage = () => {
-    const newMessage = {id: state.profilePage.messages.length+1, message: state.profilePage.newMessageText}
-    state.profilePage.messages.push(newMessage)
-    state.profilePage.newMessageText = ''
-    rerenderEntireTree(state)
-}
-export const updateNewMessageText = (newMessage: string) => {
-    state.profilePage.newMessageText = newMessage
-    rerenderEntireTree(state)
-}
 
-export const subscribe = (observer: (state: StatePropsType) => void) => {
-    rerenderEntireTree = observer
-}
+
+// export let state = <StatePropsType>{
+//     profilePage: <ProfilePageType>{
+//         DialogsData: <Array<DialogsDataType>>[
+//             {id: 1, name: 'Alex'},
+//             {id: 2, name: 'Svetlana'},
+//             {id: 3, name: 'Burton'},
+//             {id: 4, name: 'Olaf'},
+//             {id: 5, name: 'Olga'},
+//             {id: 6, name: 'Nikita'},
+//             {id: 7, name: 'Brendon'},
+//         ],
+//         messages: <Array<MessagesType>>[
+//             {id: 1, message: 'Hello my friend'},
+//             {id: 2, message: 'What are you doing?'},
+//             {id: 3, message: 'What time is it?'},
+//             {id: 4, message: 'Do you have any problems?'},
+//             {id: 5, message: 'What did you say?'},
+//         ],
+//         newMessageText: 'Enter your message'
+//     },
+//     postsPage: <PostsPageType>{
+//         PostsData: <Array<PostsDataType>>[
+//             {id: 1, message: 'Hello, it is my first post', likeCount: 23},
+//             {id: 2, message: 'Where are you?', likeCount: 56},
+//             {id: 3, message: 'I need new phone', likeCount: 2},
+//             {id: 4, message: 'What did you say?', likeCount: 99},
+//         ],
+//         newPostText: 'Enter your post'
+//     },
+//     siteBar: <Array<SiteBarType>>[
+//         {id: 1, name: 'Tom'},
+//         {id: 2, name: 'Clark'},
+//         {id: 3, name: 'Kent'},
+//     ]
+// }
+//
+// export const addPost = () => {
+//     const newPost =  {id:  state.postsPage.PostsData.length+1, message: state.postsPage.newPostText, likeCount: 0}
+//     state.postsPage.PostsData.push(newPost)
+//     // updateNewPostText('')
+//     state.postsPage.newPostText = ''
+//     rerenderEntireTree(state)
+// }
+// export const updateNewPostText = (newText: string) => {
+//     state.postsPage.newPostText = newText
+//     rerenderEntireTree(state)
+// }
+//
+// export const addMessage = () => {
+//     const newMessage = {id: state.profilePage.messages.length+1, message: state.profilePage.newMessageText}
+//     state.profilePage.messages.push(newMessage)
+//     state.profilePage.newMessageText = ''
+//     rerenderEntireTree(state)
+// }
+// export const updateNewMessageText = (newMessage: string) => {
+//     state.profilePage.newMessageText = newMessage
+//     rerenderEntireTree(state)
+// }
+//
+// export const subscribe = (observer: (state: StatePropsType) => void) => {
+//     rerenderEntireTree = observer
+// }
