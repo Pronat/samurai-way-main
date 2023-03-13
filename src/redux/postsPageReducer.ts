af7be86929ebd386c0f87448f1276c7a7173f638
AlexPron <Fotolinza@gmail.com>
2023-03-13 22:28:34 +0300

    less40-2

---
+++ src/redux/postsPageReducer.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import {ActionsType, PostsPageType} from "./state";
+
+export const PostsPageReducer = (state: PostsPageType, action: ActionsType) => {
+    if (action.type === 'ADD-POST') {
+        const newPost =  {id: state.PostsData.length+1, message: state.newPostText, likeCount: 0}
+        state.PostsData.push(newPost)
+        state.newPostText = ''
+    }
+    else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        state.newPostText = action.newText
+    }
+
+
+    return state
+
+
+};
