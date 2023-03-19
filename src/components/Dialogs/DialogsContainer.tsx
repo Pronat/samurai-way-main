d694dbb6698e6ace17e98949245e24451d024b09
AlexPron <Fotolinza@gmail.com>
2023-03-19 09:52:01 +0300

    less44-4

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -20,16 +20,27 @@ const DialogsContainer = (props: DialogsContainerPropsType) => {
     return (
         <StoreContext.Consumer>
             {
-                (store) => (
-                <Dialogs
-                updNewMessage={(messageText) => onChangeNewMessageText(messageText)}
-                addMessage={onClickAddMessageHandler}
-                messages={props.store.getState().profilePage.messages}
-                newMessageText={props.store.getState().profilePage.newMessageText}
-                dispatch={props.store.dispatch}
-                DialogsData={props.store.getState().profilePage.DialogsData}
-            />
-                )
+                (store) => {
+                    const onClickAddMessageHandler = () => {
+                        store.dispatch(addMessageActionCreator())
+                    }
+                    const onChangeNewMessageText = (messageText: string) => {
+                       store.dispatch(updNewMessageActionCreator(messageText))
+                    }
+                    return (
+                        <Dialogs
+                            updNewMessage={(messageText) => onChangeNewMessageText(messageText)}
+                            addMessage={onClickAddMessageHandler}
+                            messages={store.getState().profilePage.messages}
+                            newMessageText={store.getState().profilePage.newMessageText}
+                            dispatch={store.dispatch}
+                            DialogsData={store.getState().profilePage.DialogsData}
+                        />
+                    )
+                }
+
+
+
             }
         </StoreContext.Consumer>
     );
