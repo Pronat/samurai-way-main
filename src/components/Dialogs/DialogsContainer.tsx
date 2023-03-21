563fe7dacfa355906b94c9c04d97878f61fd3267
AlexPron <Fotolinza@gmail.com>
2023-03-21 23:03:38 +0300

    less45-8

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {addMessageActionCreator, updNewMessageActionCreator} from "../../redux/profilePageReducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
-import {ActionsType, DialogsDataType, MessagesType, StatePropsType} from "../../redux/store";
+import {DialogsDataType, MessagesType} from "../../redux/store";
 import {AppStateType} from "../../redux/redux-store";
+import {Dispatch} from "redux";
 
 // type DialogsContainerPropsType = {
 // }
@@ -52,7 +53,7 @@ let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
         newMessageText: state.profilePage.newMessageText,
     }
 }
-let mapDispatchToProps = (dispatch: (action: ActionsType) => void) => {
+let mapDispatchToProps = (dispatch: Dispatch) => {
     return {
         updNewMessage: (messageText: string) => {dispatch(updNewMessageActionCreator(messageText))},
         addMessage: () => {dispatch(addMessageActionCreator())},
--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfoContainer.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfoContainer.tsx
@@ -1,42 +1,39 @@
 import React from "react";
-import {
-    ActionsType, StatePropsType,
-} from "../../../../redux/store";
 import {addPostActionCreator, updNewPostActionCreator} from "../../../../redux/postsPageReducer";
 import {ProfileInfo} from "./ProfileInfo";
 import {connect} from "react-redux";
 import {AppStateType} from "../../../../redux/redux-store";
+import {Dispatch} from "redux";
 
 // export type ProfileInfoContainerType = {
 //     store: StoreRootStateType
 // }
+//
+const ProfileInfoContainer = (props: ProfileInfoContainerType) => {
+    const onClickAddPostHandler = () => {
+        props.store.dispatch(addPostActionCreator())
 
-// const ProfileInfoContainer = (props: ProfileInfoContainerType) => {
-//     const onClickAddPostHandler = () => {
-//         props.store.dispatch(addPostActionCreator())
-//
-//     }
-//     const onPostChange = (text: string) => {
-//
-//         props.store.dispatch(updNewPostActionCreator(text))
-//     }
-//
-//     return (
-//         <ProfileInfo
-//             updNewPostText={onPostChange}
-//             addPost={onClickAddPostHandler}
-//             newPostText={props.store.getState().postsPage.newPostText}
-//         />
-//     )
-// }
+    }
+    const onPostChange = (text: string) => {
 
-let mapStateToProps = (state: AppStateType) => {
+        props.store.dispatch(updNewPostActionCreator(text))
+    }
+    return (
+        <ProfileInfo
+            updNewPostText={onPostChange}
+            addPost={onClickAddPostHandler}
+            newPostText={props.store.getState().postsPage.newPostText}
+        />
+    )
+}
+
+let mapStateToProps2 = (state: AppStateType) => {
     debugger
     return {
         newPostText: state.postsPage.newPostText
     }
 }
-let mapDispatchToProps = (dispatch: (action: ActionsType) => void) => {
+let mapDispatchToProps2 = (dispatch: Dispatch) => {
     return {
         updNewPostText: (text: string) => {dispatch(updNewPostActionCreator(text))},
         addPost:() => {dispatch(addPostActionCreator())},
@@ -44,5 +41,5 @@ let mapDispatchToProps = (dispatch: (action: ActionsType) => void) => {
 }
 }
 
-const ProfileInfoContainer = connect(mapStateToProps, mapDispatchToProps)(ProfileInfo)
+const ProfileInfoContainer = connect(mapStateToProps2, mapDispatchToProps2)(ProfileInfo)
 export default ProfileInfoContainer
