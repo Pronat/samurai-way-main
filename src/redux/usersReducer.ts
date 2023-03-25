25026de055ce0894934e66adda125422f8609a0d
AlexPron <Fotolinza@gmail.com>
2023-03-25 12:39:51 +0300

    less49-4

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,45 +1,46 @@
 import React from 'react';
-import {
-    ActionAddMessageType,
-    ActionsType,
-    ActionUpdNewMessType,
-    DialogsDataType,
-    MessagesType,
-    ProfilePageType
-} from "./store";
-import {message} from "antd";
-
-export const addMessageActionCreator = (): ActionAddMessageType => ({type: "ADD-MESSAGE"})
-export const updNewMessageActionCreator = (newMessage: string): ActionUpdNewMessType => {
-    return {
-        type: "UPDATE-NEW-MESSAGE-TEXT", newMessage: newMessage
+type ActionsType =
+    | ReturnType<typeof followAC>
+    | ReturnType<typeof unFollowAC>
+export type UsersType = {
+    id: number
+    followed: boolean
+    FullName: string
+    status: string
+    location: {
+        city: string
+        country: string
     }
 }
+
 let initialState = {
-    DialogsData: <Array<DialogsDataType>>[
-        {id: 1, name: 'Alex'},
-        {id: 2, name: 'Svetlana'},
-        {id: 3, name: 'Burton'},
-        {id: 4, name: 'Olaf'},
-        {id: 5, name: 'Olga'},
-        {id: 6, name: 'Nikita'},
-        {id: 7, name: 'Brendon'},
+    users: <Array<UsersType>>[
+        {id: 1, followed: false, FullName: 'Alex', status: 'He is a director', location: {city: 'Minsk', country: 'Belarus'}},
+        {id: 2, followed: false, FullName: 'Svetlana', status: 'He is a worker', location: {city: 'Minsk', country: 'US'}},
+        {id: 3, followed: false, FullName: 'Nikita', status: 'He is a programmer', location: {city: 'Minsk', country: 'Germany'}},
+        {id: 4, followed: true, FullName: 'Brendon', status: 'He is a crafter', location: {city: 'Minsk', country: 'Poland'}},
+        {id: 5, followed: false, FullName: 'Olga', status: 'He is a d designer', location: {city: 'Minsk', country: 'France'}},
+        {id: 6, followed: true, FullName: 'Olga', status: 'He is a engineer', location: {city: 'Minsk', country: 'Kazakhstan'}},
     ],
-    messages: <Array<MessagesType>>[
-        {id: 1, message: 'Hello my friend'},
-        {id: 2, message: 'What are you doing?'},
-        {id: 3, message: 'What time is it?'},
-        {id: 4, message: 'Do you have any problems?'},
-        {id: 5, message: 'What did you say?'},
-    ],
-    newMessageText: 'Enter your message'
+
 }
 
-const usersReducer = (state: ProfilePageType = initialState, action: ActionsType) => {
+const usersReducer = (state: UsersType = initialState, action: ActionsType) => {
     switch (action.type) {
 
         default :
             return state
     }}
+
+export const followAC = (followed: boolean) => {
+    return{
+        type: 'FOLLOW', followed
+    }
+}
+export const unFollowAC = (followed: boolean) => {
+    return{
+        type: 'FOLLOW', followed
+    }
+}
 
 export default usersReducer
