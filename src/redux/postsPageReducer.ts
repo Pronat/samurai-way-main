9f91d2d52a42d3294620d4b39bd7f8b9d6228b66
AlexPron <Fotolinza@gmail.com>
2023-03-25 11:33:56 +0300

    less48-2

--- src/redux/postsPageReducer.ts
+++ src/redux/postsPageReducer.ts
@@ -21,9 +21,9 @@ const postsPageReducer = (state: PostsPageType = initialState, action: ActionsTy
     switch (action.type) {
         case "ADD-POST":
             const newPost = {id: state.PostsData.length + 1, message: state.newPostText, likeCount: 0}
-            state.newPostText = ''
+            // state.newPostText = ''
             return {
-                ...state, PostsData: [newPost, ...state.PostsData]
+                ...state, newPostText: '' ,PostsData: [newPost, ...state.PostsData]
             }
 
         case "UPDATE-NEW-POST-TEXT":
