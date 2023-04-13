bfd71206106337155171e37d91b6963868be537d
AlexPron <Fotolinza@gmail.com>
2023-04-13 21:48:58 +0300

    less55-2

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -24,6 +24,13 @@ class Users extends React.Component<UsersPropsType> {
     render () {
         return (
         <div>
+            <div>
+                <span>1</span>
+                <span className={s.selectedPage}>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+            </div>
             {
                 this.props.users.map(el => < div key={el.id}>
                    <span>
